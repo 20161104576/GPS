58309f5e92345b19edb17b65a74030ad62dc6f5a
20161104576 <1575726623@qq.com>
2017-06-27 08:13:48 +0800

    GPS
     更改了纬度

--- GPS/main.c
+++ GPS/main.c
@@ -7,9 +7,52 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc,const char * argv[]){
+    FILE *fp;
+    char a[80],latitude[9],longitude[10],time[7],speed[6],date[7],azi[6];
+    char b[80];
+    int altitude[5];
+    
+    int i;
+    fp=fopen("//Users//20161104576fjh//Desktop//GPS170510.log","r+");
+    fscanf(fp,"%s%s",a,b);
+    printf("结果:%s\n%s\n",a,b);
+    for(i=0;i<8;i++)
+    {
+        latitude[i]=a[i+16];
+    }
+    printf("纬度:%s\n",latitude);
+    for(i=0;i<9;i++)
+    {
+        longitude[i]=a[i+27];
+    }
+    printf("经度:%s\n",longitude);
+    for(i=0;i<6;i++)
+    {
+        time[i]=a[i+7];
+    }
+    printf("时间:%s\n",time);
+    for(i=0;i<5;i++)
+    {
+        speed[i]=a[i+39];
+    }
+    printf("速度:%s\n",speed);
+    for(i=0;i<6;i++)
+    {
+        date[i]=a[i+51];
+    }
+    printf("日期:%s\n",date);
+    for(i=0;i<5;i++)
+    {
+        azi[i]=a[i+45];
+    }
+    printf("航向:%s\n",azi);
+    for(i=0;1<4;i++)
+    {
+        altitude[i]=b[i+43]-48;
+    }
+    printf("高度:%d\n",altitude[i]);
+        fclose(fp);
     return 0;
+    
 }
